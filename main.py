b08b2318854e959e41b790b03669a5bb2902b421
cre <ncr2201030005@edu.nca.ac.jp>
2025-06-09 11:29:49 +0900

    feat: Lチカプログラムの修正と動作確認

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from mytestproject!")
+from gpiozero import MCP3008
+
+from time import sleep
 
 
-if __name__ == "__main__":
-    main()
+
+# MCP3008?ch0????
+
+adc = MCP3008(channel=0)
+
+
+
+while True:
+
+    # ??0.0?1.0?????????
+
+    value = adc.value
+
+    print("value: {:.3f}".format(value))
+
+    sleep(1)  # 1???
